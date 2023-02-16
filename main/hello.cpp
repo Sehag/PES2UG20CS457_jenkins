c206c4a2fbfdddbd984393b9ed7f9afeeaa58cc8
Sehag <85938031+Sehag@users.noreply.github.com>
2023-02-16 09:46:11 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run. Have a good day\n";
+  cout << "Continous integration using webhooks succeeded\n";
+  cout << "Have a good day\n";
   return 0;
 }
